18dfd17b046a075d108866343798e1afbbff18e6
yashmasalge <myash2101@gmail.com>
2021-11-21 22:59:58 +0530

    additional changes in launches and post-card

--- lib/widgets/launches_card_list.dart
+++ lib/widgets/launches_card_list.dart
@@ -15,22 +15,25 @@ class _LaunchesCardState extends State<LaunchesCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListTile(
-        leading: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(color:Colors.black)
+      child: InkWell(
+        onTap: () {},
+        child: (ListTile(
+          leading: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.black)),
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              maxRadius: 23,
+              foregroundImage: widget.spaceXLaunchesModel.smallIcon == null
+                  ? null
+                  : NetworkImage(widget.spaceXLaunchesModel.smallIcon!),
+            ),
           ),
-          child: CircleAvatar(
-            backgroundColor: Colors.white,
-            maxRadius: 23,
-            foregroundImage:
-               widget.spaceXLaunchesModel.smallIcon == null ? null : NetworkImage(widget.spaceXLaunchesModel.smallIcon!),
-          ),
-        ),
-        title: Text(widget.spaceXLaunchesModel.name ?? ""),
-        subtitle: Text(DateTimeUtils.getFormatedDateTimeFromEpoch(
-            (widget.spaceXLaunchesModel.dateInUnix! * 1000))),
+          title: Text(widget.spaceXLaunchesModel.name ?? ""),
+          subtitle: Text(DateTimeUtils.getFormatedDateTimeFromEpoch(
+              (widget.spaceXLaunchesModel.dateInUnix! * 1000))),
+        )),
       ),
     );
   }
